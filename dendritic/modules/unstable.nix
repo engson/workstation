322ab70e90f08c6e523ecf2c86313281a66c8016
engson <corastweb94@hotmail.com>
2026-09-19 14:50:58 +0200

    Fix system renaming

--- dendritic/modules/unstable.nix
+++ dendritic/modules/unstable.nix
@@ -1,9 +1,9 @@
-# /dendritic/modules/unstable.nix
 { inputs, ... }: {
   # This registers a global NixOS module that all your machines will inherit
   flake.nixosModules.unstable = { pkgs, ... }: {
     _module.args.pkgsUnstable = import inputs.nixpkgs-unstable {
-      inherit (pkgs) system;
+      system = pkgs.stdenv.hostPlatform.system;
+      # inherit (pkgs) system;
       config = { allowUnfree = true; };
     };
   };
