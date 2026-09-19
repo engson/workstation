bf3c58d3ccc746fc5170c502f4d470cfcf65021e
engson <corastweb94@hotmail.com>
2026-09-19 16:14:27 +0200

    Test scopeline

--- .config/helix/init.scm
+++ .config/helix/init.scm
@@ -1,3 +1,4 @@
+;; grove https://github.com/ivoronin/grove.hx
 (require "grove/grove.scm")
 (require "helix/keymaps.scm")
 
@@ -12,7 +13,8 @@
       [else (loop (cdr args))])))
 
 (grove-start!
-  #:visibility 'always)
+  #:visibility
+  (if (grove-workspace-launch?) 'always 'focused))
 
 (keymap (global)
   (normal
@@ -20,7 +22,7 @@
       (e ":grove-focus!")
       (E ":grove-visibility-toggle!"))))
 
-
+;; hx-tmux-navigator https://github.com/piotrkwarcinski/hx-tmux-navigator
 (require (prefix-in navigator. "hx-tmux-navigator/navigator.scm"))
 
 (keymap (global)
@@ -34,3 +36,7 @@
       (C-l ":navigator.move-right")
       (C-j ":navigator.move-down")
       (C-k ":navigator.move-up")))
+
+;; scoopline https://github.com/Ra77a3l3-jar/scopeline.hx
+(require "scopeline/scopeline.scm")
+; (scopeline-configure! #:position 'top-left)
--- dendritic/modules/unstable.nix
+++ dendritic/modules/unstable.nix
@@ -1,10 +1,15 @@
-{ inputs, ... }: {
+{ inputs, ... }:
+{
   # This registers a global NixOS module that all your machines will inherit
-  flake.nixosModules.unstable = { pkgs, ... }: {
-    _module.args.pkgsUnstable = import inputs.nixpkgs-unstable {
-      system = pkgs.stdenv.hostPlatform.system;
-      # inherit (pkgs) system;
-      config = { allowUnfree = true; };
+  flake.nixosModules.unstable =
+    { pkgs, ... }:
+    {
+      _module.args.pkgsUnstable = import inputs.nixpkgs-unstable {
+        system = pkgs.stdenv.hostPlatform.system;
+        # inherit (pkgs) system;
+        config = {
+          allowUnfree = true;
+        };
+      };
     };
-  };
 }
